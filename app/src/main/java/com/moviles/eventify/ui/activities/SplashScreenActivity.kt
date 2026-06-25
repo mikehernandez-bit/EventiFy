98328490783aefcdff5c4ba65c531553d3cc6adf
HERNANDEZ CHALA MIKE JHOAN <ing.mikehernandez@gmail.com>
2026-06-25 11:58:21 -0500

    Add Internet permission and configure SplashScreenActivity transition to MainActivity

--- app/src/main/java/com/moviles/eventify/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/moviles/eventify/ui/activities/SplashScreenActivity.kt
@@ -1,10 +1,14 @@
 package com.moviles.eventify.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.moviles.eventify.MainActivity
 import com.moviles.eventify.R
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -17,5 +21,12 @@ class SplashScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Delay 2 seconds, then transition to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Prevent returning to the splash screen when pressing back button
+        }, 2000)
     }
 }
